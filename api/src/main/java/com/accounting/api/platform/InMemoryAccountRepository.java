dd7b3391516cf9677a91874a9a28713c48727a3f
Alejandro Herrera <alejandro.herrera@pedidosya.com>
2020-09-05 17:30:11 -0300

    add read write lock

--- api/src/main/java/com/accounting/api/platform/InMemoryAccountRepository.java
+++ api/src/main/java/com/accounting/api/platform/InMemoryAccountRepository.java
@@ -5,17 +5,34 @@ import com.accounting.api.domain.account.models.Account;
 import org.springframework.stereotype.Component;
 
 import java.util.Optional;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 @Component
 public class InMemoryAccountRepository implements AccountRepository {
 
+    ReadWriteLock readWriteLock = new ReentrantReadWriteLock();
+
     private Account account;
 
     public Optional<Account> get() {
-        return Optional.of(account);
+        Lock readLock = readWriteLock.readLock();
+        try {
+            readLock.lock();
+            return Optional.of(account);
+        } finally {
+            readLock.unlock();
+        }
     }
 
     public void save(Account account) {
-        account = account;
+        Lock writeLock = readWriteLock.writeLock();
+        try {
+            writeLock.lock();
+            this.account = account;
+        } finally {
+            writeLock.unlock();
+        }
     }
 }
